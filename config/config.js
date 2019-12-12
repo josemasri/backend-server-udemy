24eefec039eadbb9dd8492e698346419ef88df85
Jose Masri <josemasri222@gmail.com>
2019-12-11 20:50:26 -0600

    Seccion 12 terminada - Backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 's28HjfpVkg3p8XY9jzdTyrYgpyvRBMVnkYptphCxnHKEF2AFjtYQTHag';
+// Google
+module.exports.CLIENT_ID = '858801634880-9p5pd7b4m1u9t08m21qn70g65d47pt0u.apps.googleusercontent.com';
+module.exports.SECRET_ID = '858801634880-9p5pd7b4m1u9t08m21qn70g65d47pt0u.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+// Google
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+
+
 
 var SEED = require('../config/config').SEED;
 
@@ -11,6 +17,107 @@ var app = express();
 // Schema de usuarios
 var Usuario = require('../models/usuario');
 
+// =======================================
+// Autenticacion Google
+// =======================================
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    };
+}
+app.post('/google', async(req, res) => {
+    var token = req.body.token;
+    var googleUser = await verify(token)
+        .catch(e => {
+            res.status(403).json({
+                ok: false,
+                message: 'Token no valido'
+            });
+        });
+
+
+    Usuario.findOne({ email: googleUser.email })
+        .exec((err, usuarioDB) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error buscando usuario',
+                    errors: err
+                });
+            }
+            if (usuarioDB) {
+                if (!usuarioDB.google) {
+                    return res.status(400).json({
+                        ok: false,
+                        mensaje: 'Debe usar su autenticación normal',
+                        errors: err
+                    });
+                } else {
+                    var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+
+                    res.status(200).json({
+                        ok: true,
+                        usuario: usuarioDB,
+                        token: token,
+                        id: usuarioDB.id
+                    });
+                }
+            }
+            // El usuario no existe
+            else {
+                var usuario = new Usuario();
+                usuario.nombre = googleUser.nombre;
+                usuario.email = googleUser.email;
+                usuario.img = googleUser.img;
+                usuario.google = true;
+                usuario.password = ':)';
+
+                usuario.save((err, usuarioDB) => {
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            mensaje: 'Error creando  usuario',
+                            errors: err
+                        });
+                    }
+                    var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+
+                    res.status(200).json({
+                        ok: true,
+                        usuario: usuarioDB,
+                        token,
+                        id: usuarioDB._id
+                    });
+                });
+            }
+        });
+
+
+    // res.status(200).json({
+    //     ok: true,
+    //     message: 'OK!!',
+    //     googleUser
+    // });
+});
+
+
+
+// =======================================
+// Autenticación normal
+// =======================================
 app.post('/', (req, res) => {
     var body = req.body;
 
@@ -52,9 +159,9 @@ app.post('/', (req, res) => {
             id: usuarioDB.id
         });
     });
-
-
 });
+
+
 
 
 module.exports = app;
